1c4a0729a86ca7ecc27d2cb74d29e731883bf4c7
Jose Arteaga <arteaga.jose20299@gmail.com>
2019-03-07 00:05:10 -0700

    did movement stuff (incomplete)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById('board');
 var ctx = canvas.getContext('2d');
+document.addEventListener("keydown", keyPush);
 
 var grid = [];
 var BOARD_WIDTH = 50;
@@ -16,6 +17,7 @@ var snake1X;
 var boxY;
 var alive = true;
 var snake_color = 'rgb(200,30,100)';
+var FRUIT_COLOR = '#FF9000';
 
 var time  = 0;
 var inter = setInterval(interv, 1000);
@@ -72,54 +74,52 @@ var snake1X;
 var snake1Y;
 var snake2X;
 var snake2Y;
+var fruitX;
+var fruitY;
+var tail = 5;
+var trail = [];
 
+//snake velocity
+xv = yv = 0;
+
+//creates starting location for snake
 function initializeSnake1Location() {
   snake1X = 4 * BLOCK_SIZE;
   snake1Y = 16 * BLOCK_SIZE;
 }
 
+//reference video: https://youtu.be/xGmXxpIj6vs
 //move the current place of the avatar
 //not yet done
 function move(){
   checkMove();
 
-  var gridX;
-  var gridY;
+  snake1X+=xv;
+  snake1Y+=yv;
   drawSquare(snake1X, snake1Y, snake_color);
-  
-  if(leftkey){
-    snake1X -= BLOCK_SIZE;
-    gridX = snake1X / BLOCK_SIZE;
-    gridY = snake1Y / BLOCK_SIZE;
-    if(grid[gridX][gridY] == 0){
-      snake1X -= BLOCK_SIZE / 25;
+
+  ctx.fillStyle = snake_color;
+  for(var i = 0; i < trail.length; i++){
+    ctx.fillRect(trail[i].x * BOARD_WIDTH, 
+    trail[i].y * BOARD_HEIGHT,
+     BOARD_SPACES - 2, BOARD_SPACES - 2);
+    if(trail[i].x == snake1X && trail[i].y == snake1Y) {
+      tail = 5;
     }
   }
-  if(rightkey){
-    snake1X += BLOCK_SIZE;
-    gridX = snake1X / BLOCK_SIZE;
-    gridY = snake1Y / BLOCK_SIZE;
-    if(grid[gridX][gridY] == 0){
-      snake1X += BLOCK_SIZE / 25;
-    }
+  trail.push({x: snake1X, y: snake1Y});
+  while (trail.length > tail) {
+    trail.shift();
   }
-  if(upkey){
-    snake1Y -= BLOCK_SIZE;
-    gridX = snake1X / BLOCK_SIZE;
-    gridY = snake1Y / BLOCK_SIZE;
-    if(grid[gridX][gridY] == 0){
-      snake1Y -= BLOCK_SIZE / 25;
-    }
-  }
-  if(downkey){
-    snake1Y += BLOCK_SIZE;
-    gridX = snake1X / BLOCK_SIZE;
-    gridY = snake1Y / BLOCK_SIZE;
-    if(grid[gridX][gridY] == 0){
-      snake1Y += BLOCK_SIZE / 25;
-    }
-  }
-  drawSquare(snake1X, snake1Y, snake_color);
+
+  //place fruit and make snek go big
+  //not working
+  // if(fruitX == snake1X && fruitY == snake1Y){
+  //   tail++;
+  //   fruitX = Math.floor(Math.random()*BOARD_SPACES);
+  //   fruitY = Math.floor(Math.random()*BOARD_SPACES);
+  // }
+  // drawSquare(fruitX, fruitY, FRUIT_COLOR);
 }
 
 //check to see if a move is possible
@@ -150,60 +150,35 @@ function interv(){
   time+=1;
 }
 
-var KEYCODE_LEFT = 37;
-var KEYCODE_UP = 38;
-var KEYCODE_RIGHT = 39;
-var KEYCODE_DOWN = 40;
+// player 2 will be using arrow keys to move
+// var KEYCODE_LEFT = 37;
+// var KEYCODE_UP = 38;
+// var KEYCODE_RIGHT = 39;
+// var KEYCODE_DOWN = 40;
 
-var leftkey = false;
-var rightkey = false;
-var upkey = false;
-var downkey = false;
+var KEYCODE_W = 87;
+var KEYCODE_A = 65;
+var KEYCODE_S = 83;
+var KEYCODE_D = 68;
 
 //event handler for key being pressed
-//moves player in direction of arrow keys while held down
-function handleKeyDown(evt) {
-  if(!evt){ var evt = window.event; }  //browser compatibility
-  
-  switch (evt.keyCode) {
-    case KEYCODE_LEFT:
-      leftkey = true;
-      return false;
-    case KEYCODE_RIGHT:
-      rightkey = true;
-      return false;
-    case KEYCODE_UP:
-      upkey = true;
-      return false;
-    case KEYCODE_DOWN:
-      downkey = true;
-      return false;
-    }
-  }
- 
-//event handler for a key being released
-//currently breaks the previous event handler. Will need revision
-function handleKeyUp(evt) {
+function keyPush(evt) {
   if(!evt){ var evt = window.event; }  //browser compatibility
   switch(evt.keyCode) {
-    case KEYCODE_LEFT:
-      leftkey = false;
-      break;
-    case KEYCODE_RIGHT:
-      rightkey = false;
-      break;
-    case KEYCODE_UP:
-      upkey = false;
-      break;
-    case KEYCODE_DOWN:
-      downkey = false;
-      break;
+      case KEYCODE_A:
+          xv=-BLOCK_SIZE;yv=0;
+          break;
+      case KEYCODE_W:
+          xv=0;yv=-BLOCK_SIZE;
+          break;
+      case KEYCODE_D:
+          xv=BLOCK_SIZE;yv=0;
+          break;
+      case KEYCODE_S:
+          xv=0;yv=BLOCK_SIZE;
+          break;
   }
 }
-
-document.onkeydown = handleKeyDown;
-document.onkeyup = handleKeyUp;
-
 ////////////////////////////////////////////////////////////////////////////////////
 //initializes the grid
 for(var i = 0; i<BOARD_WIDTH; i++){
